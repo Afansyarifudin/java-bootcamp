ab545b78490a6f2e65c8fc63135a0597c5bbaf18
afansyarifudin <afan.syarifudin10@gmail.com>
2023-09-15 14:06:23 +0700

    Add Challenge_2 and fix sonarlint issue

---
+++ Challenge_2/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // create new instance of CashierApp class
+        CashierApp cashierApp = new CashierApp();
+
+        // Run the program
+        cashierApp.run();
+    }
+}
